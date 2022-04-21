0c26093dabddc818a2a78bd7a523250e0c600fe3
한광수 <hankk20@naver.com>
2022-04-22 00:07:04 +0900

    10장 코딩

---
+++ src/10_todo/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoTemplate from "./component/TodoTemplate";
+import TodoInsert from "./component/TodoInsert";
+
+const App = () => {
+    return (
+        <TodoTemplate>
+            <TodoInsert/>
+        </TodoTemplate>
+    )
+}
+export default App;
